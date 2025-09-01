fd32027aba96ccc8bc04edd18c2796ea2bd8ea38
Nishita-codes <nishitapatidar03@gmail.com>
2025-09-02 01:30:24 +0530

    added files

--- app.js
+++ app.js
@@ -10,21 +10,21 @@ const mysql_db = await mysql.createConnection({
 console.log("mysql connect succesfully");
 
 // we need to create database first
-// ----------------await mysql_db.execute(`create database if not exists mysql_db`);
-// ----------------console.log(await mysql_db.execute("show databases"));
+await mysql_db.execute(`create database if not exists mysql_db`);
+console.log(await mysql_db.execute("show databases"));
 
 // then we have to create a table
-//------------- await mysql_db.execute(`
-//     create table users (
-//     id int not null auto_increment primary key,
-//     username varchar(100) not null,
-//     email varchar(100) not null unique);
-//     `);
+ await mysql_db.execute(`
+    create table users (
+    id int not null auto_increment primary key,
+    username varchar(100) not null,
+    email varchar(100) not null unique);
+    `);
 
     // inserting data into table
-//  await mysql_db.execute(`
-//         insert into users(username,email) values('vinod','vinod@thapa1.com')
-//         `);
+ await mysql_db.execute(`
+        insert into users(username,email) values('vinod','vinod@thapa1.com')
+        `);
 
   
     // READ 
@@ -33,12 +33,12 @@ console.log("mysql connect succesfully");
 
 
     // UPDATE
-    // try{
-    //     const [rows] = await mysql_db.execute(`update users set username = "nishita" where email='vinod@thapa.com' ` );
-    //     console.log("all users:" , rows);
-    // }catch(err){
-    //     console.error(err);
-    // }
+    try{
+        const [rows] = await mysql_db.execute(`update users set username = "nishita" where email='vinod@thapa.com' ` );
+        console.log("all users:" , rows);
+    }catch(err){
+        console.error(err);
+     }
 
 
 
